2d36ea0463362792d4a037ab2602290b7b0800a7
anamritraj <anandamritraj904@gmail.com>
2018-12-25 13:56:33 +0530

    Add CNAME check to prevent overriding the custom domain name.

--- script/deploy-to-gh-pages.sh
+++ script/deploy-to-gh-pages.sh
@@ -6,6 +6,9 @@ git config --global user.name "Travis CI"
 
 # deploy
 cd ./public
+if [[ -z "${custom_domain}" ]]; then
+    echo "${custom_domain}" > CNAME
+fi
 git init
 git add .
 git commit -m "Deploy to Github Pages"
