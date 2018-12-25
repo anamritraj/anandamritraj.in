a2c479e5c56c0afe4494508ac3b7fc709352e935
anamritraj <anandamritraj904@gmail.com>
2018-12-25 12:45:44 +0530

    Testing travis 2

---
+++ script/deploy-to-gh-pages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+
+# config
+git config --global user.email "someone@travis.org"
+git config --global user.name "Travis CI"
+
+# deploy
+cd ../public
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${github_token}@$github.com/${repo}.git" master:gh-pages > /dev/null 2>&1
